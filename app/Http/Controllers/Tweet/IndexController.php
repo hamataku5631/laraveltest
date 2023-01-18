592cb373239aa0bb07f49557da7c4ddf0c807f30
hamataku <sltk6ao7@gmail.com>
2023-01-18 16:34:06 +0900

    firstcommit

---
+++ app/Http/Controllers/Tweet/IndexController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Tweet;
+
+use App\Http\Controllers\Controller;
+use App\Models\Tweet;
+use Illuminate\Http\Request;
+
+class IndexController extends Controller
+{
+    public function __invoke(Request $request)
+    {
+        $tweets = Tweet::all();
+
+        //dd($tweets);
+        return view('tweet.index')
+            ->with('tweets', $tweets);
+
+    }
+}
